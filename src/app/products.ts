1bb5bc0689a2a87b5a7c67bc17679f7d3ddd800f
Mignon Van Der Voort <mignonvdvoort@gmail.com>
2024-04-07 19:30:44 +0200

    changes descriptions

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,63 +4,72 @@ export const products: IProduct[] = [
   {
     name: 'Monstera',
     price: 40,
-    description: 'asd',
+    description:
+      'The Monstera is considered a trendsetter among houseplants. The Swiss Cheese plant with its large, striking leaves, is not only present in homes, it has also provided decoration on wallpaper, posters and clothing for some years.',
     image: './assets/images/monstera.jpg',
-    availability: false,
+    availability: true,
   },
   {
     name: 'Orchid',
     price: 30,
-    description: 'asd',
+    description:
+      'Orchids are plants that belong to the family Orchidaceae, a diverse and widespread group of flowering plants with blooms that are often colourful and fragrant. Orchids are cosmopolitan plants that are found in almost every habitat on Earth except glaciers.',
     image: './assets/images/orchid.jpg',
     availability: true,
   },
   {
     name: 'Snake Plant',
     price: 35,
-    description: 'asd',
+    description:
+      'Dracaena trifasciata is a species of flowering plant in the family Asparagaceae, native to tropical West Africa from Nigeria east to the Congo. It is most commonly known as the snake plant, Saint Georges sword, mother-in-laws tongue, and vipers bowstring hemp, among other names.',
     image: './assets/images/snake-plant.jpg',
     availability: true,
   },
   {
     name: 'Lillies',
     price: 15,
-    description: 'asd',
+    description:
+      'Lilium is a genus of herbaceous flowering plants growing from bulbs, all with large prominent flowers. They are the true lilies. Lilies are a group of flowering plants which are important in culture and literature in much of the world.',
     image: './assets/images/lillies.jpg',
-    availability: false,
+    availability: true,
   },
   {
     name: 'Palm',
     price: 40,
-    description: 'asd',
+    description:
+      'Palms are one of the best known and most widely planted tree families. They have held an important role for humans throughout much of history. ',
     image: './assets/images/palm.jpg',
     availability: true,
   },
   {
     name: 'Tulips',
     price: 15,
-    description: 'asd',
+    description:
+      'Tulips are spring-blooming perennial herbaceous bulbiferous geophytes in the Tulipa genus. Their flowers are usually large, showy, and brightly coloured, generally red, orange, pink, yellow, or white. They often have a different coloured blotch at the base of the tepals, internally.',
     image: './assets/images/tulips.jpg',
     availability: true,
   },
   {
     name: '3 Succulents',
     price: 15,
-    description: 'asd',
+    description:
+      'In botany, succulent plants, also known as succulents, are plants with parts that are thickened, fleshy, and engorged, usually to retain water in arid climates or soil conditions. The word succulent comes from the Latin word sucus, meaning "juice" or "sap".',
     image: './assets/images/succulents.jpg',
     availability: true,
   },
   {
     name: 'Bonsai Tree',
     price: 30,
-    description: 'asd',
+    description:
+      'Bonsai is the Japanese art of growing and shaping miniature trees in containers, developed from the traditional Chinese art form of penjing ',
     image: './assets/images/bonsai.jpg',
     availability: true,
   },
   {
     name: 'Pinstripe Plant',
     price: 50,
-    description: 'asd',
+    description:
+      'Goeppertia ornata is a species of perennial plant in the family known as the prayer plants. It is native to South America, and is cultivated in temperate countries as a houseplant.',
     image: './assets/images/pinstripe.jpg',
     availability: true,
   },
